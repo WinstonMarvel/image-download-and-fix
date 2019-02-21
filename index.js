f294012b4fc9950d529fba1702bf082404fbcb43
WinstonMarvel <winston@winstonmarvel.com>
2019-02-21 23:38:43 +0530

    Initial build - needs refactoring for performance

---
+++ index.js
@@ -0,0 +1,39 @@
+const cheerio = require('cheerio');
+const download = require('image-downloader');
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
+
+var fileContents = fs.readFileSync(path.resolve("./input.txt"), 'utf8');
+var $ = cheerio.load(fileContents);
+
+var outputFolder = "./images/";
+
+var asyncForEach = async function(array, callback){
+	for(var i = 0; i<array.length; i++){
+		await callback(array[i]);
+	}
+};
+
+asyncForEach($("img"), async function(elem){
+	let src = $(elem).attr("src");
+	let options = {};
+	options.url = encodeURI(src);
+	let newUrl = options.url.match(/(?!.*\/).+.(jpg|jpeg|png|svg|gif)/gi)[0].replace( /[%'_/s<>:"\/\\|?*]+/g, '' );
+	options.dest = outputFolder + newUrl;
+	try{
+		const { filename, image } = await download.image(options);
+		$(elem).attr("src", `/design/images/blog/${filename.match(/(?!.*\/).+.(jpg|png|svg|gif)/gi)[0]}`);
+    	console.log('File saved to', filename.match(/(?!.*\/).+.(jpg|png|svg|gif)/gi)[0])
+	}
+	catch(err){
+    	console.log(`Error for: newUrl`, err)
+	}
+}).then( () => {
+	fs.writeFileSync('output.txt', $.html());
+	console.log("Done");
+});
+
+
+
+
