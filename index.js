21852c31b00b9541662df2084d839bedfb853f5a
WinstonMarvel <winston@winstonmarvel.com>
2019-02-22 17:44:36 +0530

    Added color codes for console. Fixed removing of parent anchor

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const download = require('image-downloader');
 const fs = require('fs');
 const path = require('path');
 const request = require('request');
+const chalk = require('chalk');
+
+var outputFolder = "./images/";
+var reWritePath = "/design/images/blog/";
 
 var fileContents = fs.readFileSync(path.resolve("./input.txt"), 'utf8');
 var $ = cheerio.load(fileContents, { decodeEntities: false });
-var outputFolder = "./images/";
-var reWritePath = "/design/images/blog/";
 
 var asyncForEach = async function(array, callback){
 	for(var i = 0; i<array.length; i++){
@@ -24,15 +26,18 @@ asyncForEach($("img"), async function(elem){
 	try{
 		const { filename, image } = await download.image(options);
 		$(elem).attr("src", `${reWritePath}${newUrl}`);
-    	console.log('Saved: ', `${newUrl}`)
-		$(elem).parent('a').attr('href', `${reWritePath}${newUrl}`);
+    	console.log( chalk.green(`Saved: ${newUrl}`) );
+    	if( $(elem).parent('a').attr('href').match(/(?!.*\/).+.(jpg|jpeg|png|svg|gif)/gi) ){ //Replace parent if also an image
+			$(elem).parent('a').attr('href', `${reWritePath}${newUrl}`);
+    		console.log( chalk.cyan(`Replaced parent anchor ${src}`) );
+    	}
 	}
 	catch(err){
-    	console.log(`Error for: newUrl`, err)
+    	console.log( chalk.bgRed(`Error for: ${newUrl}`), chalk.red(`${err}`) );
 	}
 }).then( () => {
 	fs.writeFileSync('output.txt', $.html());
-	console.log("Done");
+	console.log( chalk.bgGreen("Done") );
 });
 
 
