b2e64cc9f79c8e31d7a0595dc3850cdc29111558
Winston <winston.marveljude@thomsonreuters.com>
2019-11-04 13:33:41 +0530

    Added chalk for logs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var reWritePath = "/design/images/blog/";
 
 var fileContents = fs.readFileSync(path.resolve("./input.txt"), 'utf8');
 var $ = cheerio.load(fileContents, { decodeEntities: false });
-
+console.log($("img").html());
 var asyncForEach = async function(array, callback){
 	for(var i = 0; i<array.length; i++){
 		await callback(array[i]);
@@ -21,13 +21,14 @@ asyncForEach($("img"), async function(elem){
 	let src = $(elem).attr("src");
 	let options = {};
 	options.url = encodeURI(src);
+	console.log(options.url);
 	let newUrl = options.url.match(/(?!.*\/).+.(jpg|jpeg|png|svg|gif)/gi)[0].replace( /[%'_/s<>:"\/\\|?*]+/g, '' );
 	options.dest = outputFolder + newUrl;
 	try{
 		const { filename, image } = await download.image(options);
 		$(elem).attr("src", `${reWritePath}${newUrl}`);
     	console.log( chalk.green(`Saved: ${newUrl}`) );
-    	if( $(elem).parent('a').attr('href').match(/(?!.*\/).+.(jpg|jpeg|png|svg|gif)/gi) ){ //Replace parent if also an image
+    	if( $(elem).parent('a').attr('href') && $(elem).parent('a').attr('href').match(/(?!.*\/).+.(jpg|jpeg|png|svg|gif)/gi) ){ //Replace parent if also an image
 			$(elem).parent('a').attr('href', `${reWritePath}${newUrl}`);
     		console.log( chalk.cyan(`Replaced parent anchor ${src}`) );
     	}
